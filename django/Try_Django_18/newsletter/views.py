6ad525b6ff77be64c9849d4af286c3bf79ea868a
Ari Marin <ari.aarno.kristian.marin@gmail.com>
2016-04-10 19:00:38 +0300

    Added context variable to view and modified template so it renders given context

--- django/Try_Django_18/newsletter/views.py
+++ django/Try_Django_18/newsletter/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 
 
 # Create your views here.
+
 # View for homepage
 def home(request):
-    return render(request, "home.html", {})
+    # Variable for title
+    title = "World"
+
+    # Check is the user authenticated and change title if is
+    if request.user.is_authenticated():
+        title = "there {0}".format(request.user)
+
+    # Context variable to use with rendering
+    context = {
+        "title": title,
+
+    }
+
+    # Return the template with context
+    return render(request, "home.html", context)
