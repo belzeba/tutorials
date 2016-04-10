33dd7c803c935458e06e7190823d856b43cb30fd
Ari Marin <ari.aarno.kristian.marin@gmail.com>
2016-04-10 17:19:05 +0300

    Added first model and migrated it to database

--- django/Try_Django_18/newsletter/models.py
+++ django/Try_Django_18/newsletter/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+# Model for Sign up
+class SignUp(models.Model):
+    email = models.EmailField()
+    # Maximum of 120 characters, can be blank in form and null in database
+    full_name = models.CharField(max_length=120, blank=True, null=True)
+    # Add 'now' time on insert but not on update
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    # Add 'now' time on update but not on insert
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    # What to return when we print this object
+    def __str__(self):  # Python 2.7.* is __unicode__
+        return self.email
