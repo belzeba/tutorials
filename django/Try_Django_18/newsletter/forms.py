9a90d5dedd4e268744a0a671bd9ff1f5ba921e6b
Ari Marin <ari.aarno.kristian.marin@gmail.com>
2016-04-10 17:52:40 +0300

    Updated forms

--- django/Try_Django_18/newsletter/forms.py
+++ django/Try_Django_18/newsletter/forms.py
@@ -1,1 +1,12 @@
 # Default file(name) to user forms
+from django import forms
+
+from .models import SignUp
+
+
+# Our custom form
+class SignUpForm(forms.ModelForm):
+
+    class Meta:
+        model = SignUp
+        fields = ['email']
