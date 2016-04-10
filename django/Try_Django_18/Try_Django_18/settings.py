225733de71cca2f8fb1d2eb3943a2568d6e86552
Ari Marin <ari.aarno.kristian.marin@gmail.com>
2016-04-10 15:53:49 +0300

    Changed app name

--- django/Try_Django_18/Try_Django_18/settings.py
+++ django/Try_Django_18/Try_Django_18/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'trydjango18',
+    'newsletter',
 ]
 
 MIDDLEWARE_CLASSES = [
--- django/Try_Django_18/trydjango18/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Trydjango18Config(AppConfig):
-    name = 'trydjango18'
