1fa20eeab8a1d4af85428cf2e3774f865d7e87db
Ari Marin <ari.aarno.kristian.marin@gmail.com>
2016-04-10 17:33:52 +0300

    Added our model to admin site with custom made admin class

--- django/Try_Django_18/newsletter/admin.py
+++ django/Try_Django_18/newsletter/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import SignUp  # Own model
+
 
 # Register your models here.
+# Own admin class
+class SignUpAdmin(admin.ModelAdmin):
+    # What data to show in admin console
+    list_display = ["__str__", "timestamp", "updated"]
+
+    class Meta:
+        model = SignUp
+
+
+# Register our model for Admin site with newly created admin class
+admin.site.register(SignUp, SignUpAdmin)
