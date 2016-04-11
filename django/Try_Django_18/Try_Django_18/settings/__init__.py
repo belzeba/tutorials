94353821125ee6b6fd8170f85ee5b2990ceae721
Ari Marin <ari.aarno.kristian.marin@gmail.com>
2016-04-11 21:21:10 +0300

    Moved further towards production settings (separate settings for local and production)

--- django/Try_Django_18/Try_Django_18/settings/__init__.py
+++ django/Try_Django_18/Try_Django_18/settings/__init__.py
@@ -1,1 +1,15 @@
+# Import base settings
 from .base import *
+
+# Local settings
+try:
+    from .local import *
+except:
+    pass
+
+
+# Production settings
+try:
+    from .production import *
+except:
+    pass
