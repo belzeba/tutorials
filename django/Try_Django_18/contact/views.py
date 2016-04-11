cde9b02a8292fc19e915511cb87b82231a78e7fc
Ari Marin <ari.aarno.kristian.marin@gmail.com>
2016-04-11 11:38:32 +0300

    Changed contact email

--- django/Try_Django_18/contact/views.py
+++ django/Try_Django_18/contact/views.py
@@ -29,7 +29,7 @@ def contact(request):
         # Assign variables for mail
         subject = "Website Contact"
         from_email = "trydjango18@no-good.com"
-        to_email = ["Taks1960@cuvox.de"]  # fake mail generated: http://www.fakemailgenerator.com
+        to_email = ["Whowd1989@rhyta.com"]  # fake mail generated: http://www.fakemailgenerator.com
         contact_message = "{0}: {1} via {2}".format(form_full_name, form_message, form_email)
 
         # Send email
